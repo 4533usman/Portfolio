2bcfcd311ac3d80ec493d1608e6eb1236b69ce90
4533usman <usmankhalil4533@gmail.com>
2023-08-27 00:09:22 +0500

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import Home from './Components/Home';
 
 function App() {
   return (
     <div className='bg-gradient-to-r from-cyan-500 to-blue-500  w-full h-screen'>
 
 
-      <Navbar />
-
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,32 +1,52 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleHandler = () => {
+        setIsOpen(isOpen ? false : true)
+        console.log(isOpen)
+    }
     return (
-        <div className='flex justify-between items-center px-20 py-3'>
-            <div className='text-white font-bold'>
-                Usman
+        <>
+            <div className='flex justify-between items-center px-2 md:px-10 py-3'>
+                <div className='text-white font-bold'>
+                    Usman
+                </div>
+                <div className='grow hidden md:block'>
+                    <nav>
+                        <div>
+                            <ul className='flex justify-evenly items-center mx-10 font-semibold text-white cursor-pointer'>
+                                <li className=' transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3'><Link href="#" className="block text-white py-2">Home</Link></li>
+                                <li className=' transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3'>About</li>
+                                <li className=' transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3'>Projects</li>
+                                <li className=' transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3'>Skills</li>
+                                <li className=' transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3'>Contact Us</li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+                <div className='flex justify-between items-center gap-5'>
+                    <button className='bg-[#12121d] rounded-lg px-3 py-3 text-white font-semibold flex justify-between items-center gap-3 text-lg'>
+                        <ion-icon name="download-outline"></ion-icon>
+                        Resume
+                    </button>
+                    <button onClick={toggleHandler} className='text-4xl text-white md:hidden'><ion-icon name="menu-outline"></ion-icon></button>
+
+                </div>
             </div>
-            <div className='grow'>
-                <nav>
-                    <div>
-                        <ul className='flex justify-evenly items-center mx-10 font-semibold text-white cursor-pointer'>
-                            <li className=' transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3'>Home</li>
-                            <li className=' transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3'>About</li>
-                            <li className=' transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3'>Projects</li>
-                            <li className=' transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3'>Skills</li>
-                            <li className=' transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3'>Contact Us</li>
-                        </ul>
+            <div className={`md:hidden ${isOpen ? 'block' : 'hidden'}`}>
+                <div className="bg-blue-500 p-4">
+                    <div className='flex justify-between items-center'>
+                        <Link href="#" className="block text-white py-2 transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg px-2 grow">Home</Link>
+                        <button onClick={toggleHandler} className='text-4xl text-white md:hidden right-0 '><ion-icon name="close-outline"></ion-icon></button>
                     </div>
-                </nav>
+                    <Link href="#" className="block text-white py-2  transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg px-2">About</Link>
+                    <Link href="#" className="block text-white py-2 transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg px-2">Services</Link>
+                    <Link href="#" className="block text-white py-2 transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg px-2">Contact</Link>
+                </div>
             </div>
-            <div>
-                <button className='bg-[#12121d] rounded-2xl px-3 py-3 text-white font-semibold text-sm flex justify-between items-center gap-3'>
-                    <ion-icon className="text-xl" name="download-outline"></ion-icon>
-                    Download Resume
-                </button>
-                
-            </div>
-        </div>
+        </>
     )
 }
 
