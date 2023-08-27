fd5901d8b5c45109356542b54a4fcc347b4a171d
4533usman <usmankhalil4533@gmail.com>
2023-08-28 00:30:31 +0500

    Adding Home Page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-const s = () => {
+const Home = () => {
   return (
-    <div>s</div>
+    <div className='flex justify-center items-center flex-col h-screen gap-5'>
+      <p className='text-lg md:text-2xl max-w-lg text-center'>Hello, I am <span className='font-bold text-2xl md:text-3xl'>Muhammad Usman Khalil</span>. I am a Full Stack Developer</p>
+      <Link to='/about' className='transition ease-in-out delay-150 duration-700 flex justify-center items-center px-4 md:px-8 py-2 md:py-4 text-lg md:text-3xl border border-solid border-black hover:text-white hover:bg-black  '>
+        View My work
+        <ion-icon name="arrow-forward-outline"></ion-icon>
+      </Link>
+    </div>
   )
 }
 
-export default s
+export default Home
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,46 +1,51 @@
 import React, { useState } from 'react'
-
+import { Link } from 'react-router-dom'
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false)
     const toggleMenu = () => {
-        setIsOpen(!isOpen);
+        setIsOpen(!isOpen ? true : false)
+        console.log(isOpen)
     };
     return (
-        <div className='flex justify-between items-center px-20 py-3'>
-            <div className='text-white font-bold'>
-                Usman
-            </div>
-            <div className='grow'>
-                <nav>
-                    <div>
-                        <ul className='flex justify-evenly items-center mx-10 font-semibold text-white cursor-pointer'>
-                            <li className=' transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3'>Home</li>
-                            <li className=' transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3'>About</li>
-                            <li className=' transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3'>Projects</li>
-                            <li className=' transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3'>Skills</li>
-                            <li className=' transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3'>Contact Us</li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-            <div className='flex justify-center gap-5 items-center'>
-                <button className='bg-[#12121d] rounded-2xl px-3 py-3 text-white font-semibold text-sm flex justify-between items-center gap-3'>
-                    <ion-icon className="text-xl" name="download-outline"></ion-icon>
-                    Download Resume
-                </button>
-                <div className='text-5xl text-white font-semibold'>
-                    <button onClick={toggleMenu}>
-                        <ion-icon name="menu-outline"></ion-icon>
+        <>
+            <div className='flex justify-between items-center px-5 md:px-10 py-3'>
+                <div className='text-white font-bold'>
+                    Usman
+                </div>
+                <div className='grow hidden md:block'>
+                    <nav>
+                        <div>
+                            <ul className='flex justify-evenly items-center mx-10 font-semibold text-white cursor-pointer'>
+                                <li className=' transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3'><Link to='/'>Home</Link></li>
+                                <li className=' transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3'><Link to='/about'>About</Link></li>
+                                <li className=' transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3'><Link to='/projects'>Projects</Link></li>
+                                <li className=' transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3'><Link to='/contact'>Contact</Link></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+                <div className='flex justify-center gap-5 items-center'>
+                    <button className='bg-[#12121d] rounded-2xl px-3 py-3 text-white font-semibold text-sm flex justify-between items-center gap-3'>
+                        <ion-icon className="text-xl" name="download-outline"></ion-icon>
+                        Download Resume
                     </button>
+                    <div className='text-5xl text-white font-semibold'>
+                        <button onClick={toggleMenu} className='block md:hidden'>
+                            <ion-icon name="menu-outline"></ion-icon>
+                        </button>
+                    </div>
                 </div>
             </div>
-            <div className={`md:flex space-x-4 ${isOpen ? 'block' : 'hidden'}`}>
-                <a href="#" className="text-white">Home</a>
-                <a href="#" className="text-white">About</a>
-                <a href="#" className="text-white">Portfolio</a>
-                <a href="#" className="text-white">Contact</a>
+            <div className={`${isOpen ? 'block' : 'hidden'} flex flex-col text-black bg-white p-5`}>
+                <button onClick={toggleMenu} className='text-5xl font-semibold'>
+                    <ion-icon name="close-outline"></ion-icon>
+                </button>
+                <Link to="/" className="transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3">Home</Link>
+                <Link to="/about" className="transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3">About</Link>
+                <Link to="/projects" className="transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3">Projects</Link>
+                <Link to="/contact" className="transition ease-in-out delay-150 duration-700 hover:text-gray-300  hover:bg-[#12121d]  rounded-lg p-3">Contact</Link>
             </div>
-        </div>
+        </>
     )
 }
 
