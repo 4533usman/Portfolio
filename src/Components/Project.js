d02e0a67cf49d83f81755191b26fb543c3e3d78c
4533usman <usmankhalil4533@gmail.com>
2023-09-01 16:59:27 +0500

    Add Projects

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
-import PortfolioImage from '../Images/Portfolio.png'
+import gimg from '../Images/download.jpeg'
 import { Link } from 'react-router-dom';
 const Project = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const data = [
+        { id: 1, name: "Usman", category: 'reactjs' },
+        { id: 2, name: "Usman2", category: 'reactjs' },
+        { id: 3, name: "Usman3", category: 'reactnative' },
+        { id: 4, name: "Usman4", category: 'reactnative' },
+        { id: 5, name: "Usman5", category: 'mern' },
+        { id: 6, name: "Usman6", category: 'mern' },
+        { id: 7, name: "Usman7", category: 'python' },
+        { id: 8, name: "Usman8", category: 'python' },
+    ]
+    const toggleDropdown = () => {
+        setIsOpen(isOpen ? false : true);
+        console.log(isOpen)
+    }
+    useEffect(() => {
+
+    }, [])
+    const [allProjects, setAllProjects] = useState(true);
+    const [reactProjects, setReactProjects] = useState(false);
+    const [reactnativeProjects, setReactNativeProjects] = useState(false);
+    const [mernProjects, setMernProjects] = useState(false);
+    const [pythonProjects, setPythonProjects] = useState(false);
+    const AllProjects = () => {
+        setMernProjects(false);
+        setReactNativeProjects(false);
+        setReactProjects(false)
+        setPythonProjects(false);
+        setAllProjects(true);
+    }
+    const AllPythonProjects = () => {
+        setAllProjects(false);
+        setMernProjects(false);
+        setReactNativeProjects(false);
+        setReactProjects(false)
+        setPythonProjects(true);
+    }
+    const AllReactNativeProjects = () => {
+        setAllProjects(false);
+        setMernProjects(false);
+        setReactProjects(false)
+        setPythonProjects(false);
+        setReactNativeProjects(true);
+    }
+    const AllReactProjects = () => {
+        setAllProjects(false);
+        setMernProjects(false);
+        setReactNativeProjects(false);
+        setPythonProjects(false);
+        setReactProjects(true)
+    }
+    const AllMernProjects = () => {
+        setAllProjects(false);
+        setReactNativeProjects(false);
+        setReactProjects(false)
+        setPythonProjects(false);
+        setMernProjects(true);
+    }
     return (
         <div className='bg-gradient-to-r from-violet-700 to-violet-500'>
             <Navbar />
@@ -12,20 +70,20 @@ const Project = () => {
             <div className=' px-2 md:px-10 hidden md:block'>
                 <nav>
                     <ul className='flex justify-evenly font-semibold text-white'>
-                        <li>
-                            <Link className='transition ease-in-out delay-150 duration-700 px-4 py-2 hover:bg-black hover:text-white'>All</Link>
+                        <li onClick={AllProjects}>
+                            <Link className={`${allProjects ? 'bg-black' : 'bg-none'} transition ease-in-out delay-150 duration-700 px-4 py-2 hover:bg-black hover:text-white`}>All</Link>
                         </li>
-                        <li>
-                            <Link className='transition ease-in-out delay-150 duration-700 px-4 py-2 hover:bg-black hover:text-white'>React Native</Link>
+                        <li onClick={AllReactNativeProjects}>
+                            <Link className={`${reactnativeProjects ? 'bg-black' : 'bg-none'}  transition ease-in-out delay-150 duration-700 px-4 py-2 hover:bg-black hover:text-white`}>React Native</Link>
                         </li>
-                        <li>
-                            <Link className='transition ease-in-out delay-150 duration-700 px-4 py-2 hover:bg-black hover:text-white'>React Js</Link>
+                        <li onClick={AllReactProjects}>
+                            <Link className={`${reactProjects ? 'bg-black' : 'bg-none'} transition ease-in-out delay-150 duration-700 px-4 py-2 hover:bg-black hover:text-white`}>React Js</Link>
                         </li>
-                        <li>
-                            <Link className='transition ease-in-out delay-150 duration-700 px-4 py-2 hover:bg-black hover:text-white'>MERN Stack</Link>
+                        <li onClick={AllMernProjects}>
+                            <Link className={`${mernProjects ? 'bg-black' : 'bg-none'} transition ease-in-out delay-150 duration-700 px-4 py-2 hover:bg-black hover:text-white`}>MERN Stack</Link>
                         </li>
-                        <li>
-                            <Link className='transition ease-in-out delay-150 duration-700 px-4 py-2 hover:bg-black hover:text-white'>Python</Link>
+                        <li onClick={AllPythonProjects}>
+                            <Link className={`${pythonProjects ? 'bg-black' : 'bg-none'} transition ease-in-out delay-150 duration-700 px-4 py-2 hover:bg-black hover:text-white`}>Python</Link>
                         </li>
                     </ul>
                 </nav>
@@ -34,11 +92,122 @@ const Project = () => {
                 </div>
             </div>
             {/* Navigation for the screen md and after */}
-            <div className='flex justify-between items-center mx-10 px-5 py-2 rounded-md  bg-white '>
-                <p>Dropdown</p>
-                <button><ion-icon className='cursor-pointer' name="chevron-down-outline"></ion-icon></button>
+            <div className=' block md:hidden'>
+                <div className='flex flex-col mx-10 py-2  px-3 rounded-md  bg-white'>
+                    <div className='flex items-center font-semibold justify-between px-3'>
+
+                        {allProjects &&
+                            <p>All</p>
+                        }
+                        {reactnativeProjects &&
+                            <p>React Native</p>
+                        }
+                        {reactProjects &&
+                            <p>React Js</p>
+                        }
+                        {mernProjects &&
+                            <p>MERN Stack</p>
+                        }
+                        {pythonProjects &&
+                            <p>Python</p>
+                        }
+
+                        <button onClick={toggleDropdown}><ion-icon className='cursor-pointer' name="chevron-down-outline"></ion-icon></button>
+                    </div>
+                    <div className={`${isOpen ? 'block' : 'hidden'}`}>
+                        <ul className='flex flex-col font-semibold text-black gap-4 mt-2 '>
+                            <li className='' onClick={() => {
+                                AllProjects()
+                                toggleDropdown()
+                            }}>
+                                <Link className='transition ease-in-out delay-150 duration-700 px-4 py-2'>All</Link>
+                            </li>
+                            <li onClick={() => {
+                                AllReactNativeProjects()
+                                toggleDropdown()
+                            }}>
+                                <Link className='transition ease-in-out delay-150 duration-700 px-4 py-2'>React Native</Link>
+                            </li>
+                            <li onClick={() => {
+                                AllReactProjects()
+                                toggleDropdown()
+                            }}>
+                                <Link className='transition ease-in-out delay-150 duration-700 px-4 py-2'>React Js</Link>
+                            </li>
+                            <li onClick={() => {
+                                AllMernProjects()
+                                toggleDropdown()
+                            }}>
+                                <Link className='transition ease-in-out delay-150 duration-700 px-4 py-2'>MERN Stack</Link>
+                            </li>
+                            <li onClick={() => {
+                                AllPythonProjects()
+                                toggleDropdown()
+                            }}>
+                                <Link className='transition ease-in-out delay-150 duration-700 px-4 py-2'>Python</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
             </div>
-        </div>
+
+            <div class="grid grid-cols-2 md:grid-cols-3 gap-4 my-10 px-2 md:px-10">
+                {allProjects &&
+                    data.map((e) => (
+                        <div className='flex flex-col rounded-lg gap-3 p-5'>
+                            <img class="h-auto max-w-full rounded-lg" src={gimg} alt="" />
+                            <p className='text-white px-2'>{e.name}</p>
+                        </div>
+                    ))
+                }
+
+                {reactProjects && (
+                    data
+                        .filter((item) => item.category === 'reactjs')
+                        .map((e) => (
+                            <div className='flex flex-col rounded-lg gap-3 p-5' key={e.id}>
+                                <img className="h-auto max-w-full rounded-lg" src={gimg} alt="" />
+                                <p className='text-white px-2'>{e.name}</p>
+                            </div>
+                        ))
+                )}
+                {reactnativeProjects &&
+                    data
+                        .filter((item) => item.category === 'reactnative')
+                        .map((e) => (
+                            <div className='flex flex-col rounded-lg gap-3 p-5' key={e.id}>
+                                <img className="h-auto max-w-full rounded-lg" src={gimg} alt="" />
+                                <p className='text-white px-2'>{e.name}</p>
+                            </div>
+                        ))
+                }
+
+                {mernProjects &&
+                    data
+                        .filter((item) => item.category === 'mern')
+                        .map((e) => (
+                            <div className='flex flex-col rounded-lg gap-3 p-5' key={e.id}>
+                                <img className="h-auto max-w-full rounded-lg" src={gimg} alt="" />
+                                <p className='text-white px-2'>{e.name}</p>
+                            </div>
+                        ))
+                }
+
+                {pythonProjects &&
+                    data
+                        .filter((item) => item.category === 'python')
+                        .map((e) => (
+                            <div className='flex flex-col rounded-lg gap-3 p-5' key={e.id}>
+                                <img className="h-auto max-w-full rounded-lg" src={gimg} alt="" />
+                                <p className='text-white px-2'>{e.name}</p>
+                            </div>
+                        ))
+                }
+
+            </div>
+
+        </div >
     )
 }
 
